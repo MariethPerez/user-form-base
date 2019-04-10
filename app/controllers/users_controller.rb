273cd18ddff01d54e7679a98afeed5814db6d16a
Marieth <mperezzevallos@gmail.com>
2019-04-10 09:26:52 -0500

    Add validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(params[:user])
+    @user = User.create(user_params)
+
+    if @user.save
+       redirect_to action: :index
+      # redirect_to  users_path
+    else
+      render :new
+    end
+      
+    # first_name: params[:username],
+    #                     last_name: params[:username],  
+    #                     username: params[:username], 
+    #                     email: params[:username],
+    #                     email_confirmation: params[:username],
+    #                     password: params[:password],
+    #                     password_confirmation: params[:password_confirmation],
+    #                     password_hint: params[:password_confirmation])
+
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,34 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
+
+
+  validates :first_name, presence: true , length: { in: 2..60 }
+  validates :last_name, presence: true , length: { in: 2..60 }
+  validates :username, presence: true , length: { in: 2..20 } , uniqueness: true
+  validates :email, presence: true , uniqueness: true, email: true , confirmation: true
+  validates :email_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 8 } , 
+                      format: { with: /\d+/, message: "must contain at least one number" }
+  validates :password_confirmation, presence: true ,confirmation: true
+  validates :password_hint, length: { maximum: 100 }
+  before_create :capitalize_names
+
+
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def capitalize_names
+    self.first_name = first_name.downcase.camelcase
+    nocaps = ["de", "la", "del"]
+    self.last_name =last_name.downcase.split.map { |word| nocaps.include?(word) ? word : word.capitalize }.join(" ")
+  end
+  
 end
